e42c7ccea64a37d1b0ee3d859aedf6a3c5342112
dominhduc20031110 <dominhduc20031110@gmail.com>
2019-11-22 16:05:37 +0700

    homework2

---
+++ s4/bai2.py
@@ -0,0 +1,8 @@
+n = int(input())
+a = 0
+for i in range(0,n):
+    b = int(input())
+    a += b
+a /= n
+
+print(a)
---
+++ s4/bai3.py
@@ -0,0 +1,7 @@
+year = int(input())
+a = int(year % 4)
+if a == 0:
+    print("The year",year ,"is a leap year.")
+else:
+    print("The year",year ,"is not a leap year.")
+ 
